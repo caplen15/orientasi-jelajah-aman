87ed7b7e12985b00c99b7c87457b112cb3e2bb6d
Hamdan Aldi <mhamdanaldiansyah@gmail.com>
2026-09-22 13:49:07 +0700

    feat: tambah navigasi dari RiwayatList

--- src/components/RiwayatList.tsx
+++ src/components/RiwayatList.tsx
@@ -1,4 +1,5 @@
 import { View, Text } from "react-native";
+import { Link } from "expo-router";
 interface RiwayatListProps {
   daftarKota: string[];
 }
@@ -6,7 +7,13 @@ export default function RiwayatList({ daftarKota }: RiwayatListProps) {
   return (
     <View>
       {daftarKota.map((kota) => (
-        <Text key={kota}>{kota}</Text>
+        // Format objek pada href lolos pengecekan TypeScript Expo Router
+        <Link
+          key={kota}
+          href={{ pathname: "/detail/[kota]", params: { kota } }}
+        >
+          <Text>{kota}</Text>
+        </Link>
       ))}
     </View>
   );
